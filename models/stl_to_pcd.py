0e0e0346b6b84ad45d0cb52076b170146db5caf1
Kirill Lalayants <lalayantskirill@gmail.com>
2023-07-04 02:01:03 +0300

    converted files added

--- models/stl_to_pcd.py
+++ models/stl_to_pcd.py
@@ -4,5 +4,5 @@ number_of_points = 100
 
 input = o3d.io.read_triangle_mesh(path)
 path = path.replace('.stl', '')
-new_pcd = o3d.geometry.sample_points_uniformly(input, number_of_points=number_of_points)
+new_pcd = o3d.geometry.TriangleMesh.sample_points_uniformly(input, number_of_points=number_of_points)
 o3d.io.write_point_cloud(f"{path}_{number_of_points}.pcd", new_pcd)
