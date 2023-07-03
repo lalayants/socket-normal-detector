6d40f9379d0bbe650b739163b0892806f76f2e62
Kirill Lalayants <lalayantskirill@gmail.com>
2023-07-04 01:48:14 +0300

    converter added

--- models/stl_to_pcd.py
+++ models/stl_to_pcd.py
@@ -1,5 +1,8 @@
 import open3d as o3d
+path = 'plug.stl'
+number_of_points = 100
 
-input = o3d.io.read_triangle_mesh("models/plug.stl")
-new_pcd = o3d.geometry.sample_points_uniformly(input, number_of_points=100)
-o3d.io.write_point_cloud("copy_of_fragment.pcd", new_pcd)
+input = o3d.io.read_triangle_mesh(path)
+path = path.replace('.stl', '')
+new_pcd = o3d.geometry.sample_points_uniformly(input, number_of_points=number_of_points)
+o3d.io.write_point_cloud(f"{path}_{number_of_points}.pcd", new_pcd)
