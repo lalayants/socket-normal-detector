5dd298baaa91e783f4bf5ac1f9ddbb6b52ffe5b3
Kirill Lalayants <lalayantskirill@gmail.com>
2023-07-09 18:03:28 +0300

    scaled models

--- models/stl_to_pcd.py
+++ models/stl_to_pcd.py
@@ -1,8 +1,10 @@
 import open3d as o3d
-path = 'plug.stl'
+path = 'models/plug.stl'
 number_of_points = 100
 
 input = o3d.io.read_triangle_mesh(path)
 path = path.replace('.stl', '')
-new_pcd = o3d.geometry.TriangleMesh.sample_points_uniformly(input, number_of_points=number_of_points)
-o3d.io.write_point_cloud(f"{path}_{number_of_points}.pcd", new_pcd)
+for number_of_points in [10000, 15000, 25000, 50000, 100000]:
+    new_pcd = o3d.geometry.TriangleMesh.sample_points_uniformly(input, number_of_points=number_of_points).scale(scale=0.001, center=[0,0,0])
+    print(new_pcd, new_pcd.get_max_bound()-new_pcd.get_min_bound())
+    o3d.io.write_point_cloud(f"{path}_{number_of_points}.pcd", new_pcd)
