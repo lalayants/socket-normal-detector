d9b4944622dce73c73d86717b210ab2abcc53943
Kirill Lalayants <lalayantskirill@gmail.com>
2023-07-10 16:00:40 +0300

    small refactoring

--- open3d_test_py/simple.py
+++ open3d_test_py/simple.py
@@ -33,7 +33,8 @@ def preprocess_point_cloud(pcd, voxel_size):
         o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
     return pcd_down, pcd_fpfh
 
-def prepare_dataset(voxel_size):
+
+def prepare_dataset(voxel_size, source, target):
     print(":: Load two point clouds and disturb initial pose.")
 
     demo_icp_pcds = o3d.data.DemoICPPointClouds()
@@ -47,6 +48,7 @@ def prepare_dataset(voxel_size):
     source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
     target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
     return source, target, source_down, target_down, source_fpfh, target_fpfh
+
 
 def execute_global_registration(source_down, target_down, source_fpfh,
                                 target_fpfh, voxel_size):
@@ -66,47 +68,58 @@ def execute_global_registration(source_down, target_down, source_fpfh,
         ], o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
     return result
 
-config_filename = 'open3d_test_py/cfg.json'
-with open(config_filename) as cf:
-    rs_cfg = o3d.t.io.RealSenseSensorConfig(json.load(cf))
-extr = np.array([
-    [1., 0., 0., 0.],
-    [0., 1., 0., 0.],
-    [0., 0., 1., 0.],
-    [0., 0., 0., 1.]])
-rs = o3d.t.io.RealSenseSensor()
-rs.init_sensor(rs_cfg, 0)
-rs.start_capture(True)  # true: start recording with capture
-# print(o3d.t.io.RealSenseSensor.list_devices())
-meta = rs.get_metadata()
-intrinsics = o3d.camera.PinholeCameraIntrinsic(meta.width, meta.height, *meta.intrinsics.get_focal_length(), *meta.intrinsics.get_principal_point())
-# target = o3d.io.read_point_cloud('models/plug_50000.pcd').scale(scale=0.001, center=[0,0,0])
-# o3d.io.write_point_cloud("plug_50000_mini.pcd", target)
-target = o3d.io.read_point_cloud('models/plugs_pcd/milk.pcd')
 
-print(target.get_max_bound()-target.get_min_bound())
-while True:
-    im_rgbd = rs.capture_frame(True, True) # wait for frames and align them
-    # print(rs.get_metadata().intrinsics.intrinsic_matrix, rs.get_metadata().depth_scale)
+def rs_get_pcd(rs, extr):
     # source = o3d.geometry.PointCloud.create_from_depth_image(im_rgbd.depth.to_legacy(), rs.get_metadata().intrinsics, depth_scale=rs.get_metadata().depth_scale, extrinsic=extr)
     # rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(im_rgbd.color.to_legacy(), im_rgbd.depth.to_legacy())
     # source = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, rs.get_metadata().intrinsics, extrinsic=extr)
     # print(source.get_max_bound()-source.get_min_bound())
-    source = o3d.io.read_point_cloud('models/scenes_pcd/milk_cartoon_all_small_clorox_fixed.pcd')
-    
-    voxel_size = 0.001  # means 5cm for this dataset
-    source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(
-        voxel_size)
+    im_rgbd = rs.capture_frame(True, True) # wait for frames and align them
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(im_rgbd.color.to_legacy(), im_rgbd.depth.to_legacy())
+    source = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, rs.get_metadata().intrinsics, extrinsic=extr)
+    print(f'RS pcd size: {source.get_max_bound()-source.get_min_bound()}')
+    return source
+
+
+def match_pcds(source, target, voxel_size = 0.001, draw = False):
+    source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(voxel_size, source, target)
     result_ransac = execute_global_registration(source_down, target_down,
                                             source_fpfh, target_fpfh,
                                             voxel_size)
-    draw_registration_result(source_down, target_down, result_ransac.transformation)
-    
-    # o3d.io.write_point_cloud("copy_of_fragment.pcd", source)
-    # o3d.visualization.draw_geometries([target])
-    #                             #       zoom=0.3412,
-    #                             #       front=[0, 0, 1],
-    #                             #   lookat=[0, 0, 0],
-    #                             #   up=[0, -1, 0])
+    if draw:
+        draw_registration_result(source_down, target_down, result_ransac.transformation)
 
-rs.stop_capture()
+if __name__ == '__main__':
+    #Realsense initialisation
+    config_filename = 'open3d_test_py/cfg.json'
+    with open(config_filename) as cf:
+        rs_cfg = o3d.t.io.RealSenseSensorConfig(json.load(cf))
+    extr = np.array([
+        [1., 0., 0., 0.],
+        [0., 1., 0., 0.],
+        [0., 0., 1., 0.],
+        [0., 0., 0., 1.]])
+    rs = o3d.t.io.RealSenseSensor()
+    rs.init_sensor(rs_cfg, 0)
+    rs.start_capture(True)  # true: start recording with capture
+    meta = rs.get_metadata()
+    intrinsics = o3d.camera.PinholeCameraIntrinsic(meta.width, meta.height, *meta.intrinsics.get_focal_length(), *meta.intrinsics.get_principal_point())
+    
+    #Read target from file
+    # target = o3d.io.read_point_cloud('models/plug_50000.pcd')
+    target = o3d.io.read_point_cloud('models/plugs_pcd/milk.pcd')
+    print(f'target size: {target.get_max_bound()-target.get_min_bound()}')
+    
+    while True:
+        source = rs_get_pcd(rs, extr)
+        source = o3d.io.read_point_cloud('models/scenes_pcd/milk_cartoon_all_small_clorox_fixed.pcd')
+        
+        match_pcds(source, target, voxel_size = 0.001, draw = True)
+        # o3d.io.write_point_cloud("copy_of_fragment.pcd", source)
+        # o3d.visualization.draw_geometries([target])
+        #                             #       zoom=0.3412,
+        #                             #       front=[0, 0, 1],
+        #                             #   lookat=[0, 0, 0],
+        #                             #   up=[0, -1, 0])
+
+    rs.stop_capture()
